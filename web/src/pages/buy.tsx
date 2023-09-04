43fc8616deaa01ca5e3c2831517b0cf17cd32b87
Sanam Limbu <sudosanam@gmail.com>
2023-09-04 12:37:18 +0800

    fix auth

--- web/src/pages/buy.tsx
+++ web/src/pages/buy.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 export default function BuyPage() {
   return (
     <>
-      <Typography>Buy</Typography>
+      <Typography>Buys</Typography>
     </>
   );
 }
