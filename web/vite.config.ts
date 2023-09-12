a66bf1e761132c4d2e469547cb28bcd61ef8073e
Sanam Limbu <sudosanam@gmail.com>
2023-09-12 17:16:01 +0800

    config vite to use process.env

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      "process.env.VITE_RENTHOME_API_ADDRESS": JSON.stringify(
+        env.VITE_RENTHOME_API_ADDRESS
+      ),
+      "process.env.VITE_RENTHOME_PUBLIC_ADDRESS": JSON.stringify(
+        env.VITE_RENTHOME_PUBLIC_ADDRESS
+      ),
+      "process.env.VITE_RENTHOME_GOOGLE_OAUTH_CLIENT_ID": JSON.stringify(
+        env.VITE_RENTHOME_GOOGLE_OAUTH_CLIENT_ID
+      ),
+      "process.env.VITE_RENTHOME_FACEBOOK_OAUTH_CLIENT_ID": JSON.stringify(
+        env.VITE_RENTHOME_FACEBOOK_OAUTH_CLIENT_ID
+      ),
+    },
+  };
+});
